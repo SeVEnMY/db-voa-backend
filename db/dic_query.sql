f506639f71a11dd47e7da14326890884d73e960b
SeVEnMY <lioneljiang77@gmail.com>
2023-05-01 13:55:00 -0400

    edit sql files

--- db/dic_query.sql
+++
@@ -1,3 +0,0 @@
-select table_name, column_name, data_type, data_length, nullable from user_tab_cols;
-select table_name, constraint_name, constraint_type from user_constraints;
-select table_name, column_name, comments from user_col_comments; 
